8ff29b567279c12d18df77ee30c44c51b720f473
Eivind Fonn <evfonn@gmail.com>
2015-11-24 00:07:44 +0100

    Initial work on notes intro

---
+++ notes/auto/01-introduction.el
@@ -0,0 +1,18 @@
+(TeX-add-style-hook
+ "01-introduction"
+ (lambda ()
+   (TeX-add-symbols
+    "arraystretch")
+   (LaTeX-add-labels
+    "fig:single"
+    "fig:fp"
+    "maxnumber"
+    "minnumber"
+    "fig:perform_dev"
+    "fig:proj_perform_dev"
+    "tab:history_ntnu"
+    "fig:njord-2"
+    "fig:njord-1"
+    "fig:met"
+    "fig:climate")))
+
