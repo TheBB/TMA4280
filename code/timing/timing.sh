da27e26a318e273f4e22073c6c2a44f497dc8573
Eivind Fonn <evfonn@gmail.com>
2015-12-13 16:18:16 +0100

    Add crash slides

--- code/timing/timing.sh
+++ code/timing/timing.sh
@@ -2,12 +2,12 @@
 
 totaltime=0
 rm -f tmp.asc
-for times in `seq 100 1000 5000`
+for ntimes in `seq 100 1000 5000`
 do
     for len in `seq 1000 10000 20000`
     do
-        ELAPSED=`./timing $times $len | grep Used | awk -F ' ' '{print $2}'`
-        let FLOP=times*len*2
+        ELAPSED=`./timing $ntimes $len | grep Used | awk -F ' ' '{print $2}'`
+        let FLOP=ntimes*len*2
         echo "$FLOP $ELAPSED" >> tmp.asc
     done
 done
