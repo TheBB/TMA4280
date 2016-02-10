4147f8382cc6ca97962d4037c9b604cf18bf6a1a
Eivind Fonn <evfonn@gmail.com>
2016-02-10 14:06:27 +0100

    Add LAPACK test

--- code/blas/main.c
+++ code/blas/main.c
@@ -6,27 +6,79 @@
 #include "cblas.h"
 #endif
 
-int main(int argc, char **argv)
+void print_matrix(char* desc, int m, int n, double* a, int lda)
+{
+    int i, j;
+    printf( "\n %s\n", desc );
+    for (i = 0; i < m; i++) {
+        for (j = 0; j < n; j++)
+            printf(" %8.2f", a[i+j*lda]);
+        printf("\n");
+    }
+}
+
+
+void print_int_vector(char* desc, int n, int* a) {
+    int j;
+    printf("\n %s\n", desc);
+    for(j = 0; j < n; j++)
+        printf(" %8i", a[j]);
+    printf("\n");
+}
+
+
+void cblas_test()
 {
     const int lda = 3, ldb = 3, ldc = 3;
     int m = 3, n = 3, k = 3;
-    float alpha = 1.0, beta = 1.0;
-    float a[] = {0.11, 0.21, 0.12,
-                 0.15, 0.13, 0.17,
-                 0.22, 0.13, 0.23};
-    float b[] = {1011, 1021, 1018,
-                 1031, 1012, 1021,
-                 1022, 1032, 1015};
-    float c[] = {0.00, 0.00, 0.00,
-                 0.00, 0.00, 0.00,
-                 0.00, 0.00, 0.00};
+    double alpha = 1.0, beta = 1.0;
+    double a[] = {0.11, 0.21, 0.12,
+                  0.15, 0.13, 0.17,
+                  0.22, 0.13, 0.23};
+    double b[] = {1011, 1021, 1018,
+                  1031, 1012, 1021,
+                  1022, 1032, 1015};
+    double c[] = {0.00, 0.00, 0.00,
+                  0.00, 0.00, 0.00,
+                  0.00, 0.00, 0.00};
 
-    cblas_sgemm(CblasColMajor, CblasNoTrans, CblasNoTrans,
+    cblas_dgemm(CblasColMajor, CblasNoTrans, CblasNoTrans,
                 m, n, k, alpha, a, lda, b, ldb, beta, c, ldc);
 
-    printf("[ %g, %g, %g\n",   c[0], c[1], c[2]);
-    printf("  %g, %g, %g\n",   c[3], c[4], c[5]);
-    printf("  %g, %g, %g ]\n", c[6], c[7], c[8]);
+    print_matrix("Multiplication result", m, n, c, 1);
+}
+
+
+void lapack_test()
+{
+    int n = 5, nrhs = 3, lda = 5, ldb = 5, info;
+
+    int ipiv[5];
+    double a[25] = {
+        6.80, -2.11,  5.66,  5.97,  8.23,
+        -6.05, -3.30,  5.36, -4.44,  1.08,
+        -0.45,  2.58, -2.70,  0.27,  9.04,
+        8.32,  2.71,  4.35, -7.17,  2.14,
+        -9.67, -5.14, -7.26,  6.08, -6.87
+    };
+    double b[15] = {
+        4.02,  6.19, -8.22, -7.57, -3.03,
+        -1.56,  4.00, -8.67,  1.75,  2.86,
+        9.81, -4.09, -4.57, -8.61,  8.99
+    };
+
+    dgesv_(&n, &nrhs, a, &lda, ipiv, b, &ldb, &info);
+
+    print_matrix("Solution", n, nrhs, b, ldb);
+    print_matrix("Details of LU factorization", n, n, a, lda);
+    print_int_vector("Pivot indices", n, ipiv);
+}
+
+
+int main(int argc, char **argv)
+{
+    cblas_test();
+    lapack_test();
 
     return 0;
 }
